ad5777f2b3e01df2af25eb1ecf9daf904909ca70
Adelaide Zhang <adelaidemzhang@gmail.com>
2023-07-10 16:14:02 -0400

    use absolute pathname for static

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ const path = require('path');
 const app = express()
 const port = 3000
 
-app.use(express.static('static'))
+app.use(express.static(path.join(__dirname, 'static')));
 // app.get('/', (req, res) => {
 //   res.send('Hello World!')
 // })
 
 
-app.use('/', (req, res, next) => {
+app.use('/', (req, res) => {
     console.log('Request URL:', req.originalUrl)
-    console.log('Request Type:', req.method)
+    console.log('Request Path:', req.path)
+    console.log('dir name:', __dirname)
     res.sendFile(path.join(__dirname+'/index.html'));
-    // next()
   })
 
 app.listen(port, () => {
