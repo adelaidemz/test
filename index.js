0f8695a1cf05527d5b120bccc8b3cdde9f4b2a64
Adelaide Zhang <adelaidemzhang@gmail.com>
2023-07-11 13:21:59 -0400

    rework dir structure

--- index.js
+++ index.js
@@ -3,16 +3,9 @@ const path = require('path');
 const app = express()
 const port = 3000
 
-app.use(express.static(path.join(__dirname, 'static')));
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
-
+app.use(express.static(path.join(__dirname, '/')));
 
 app.use('/', (req, res) => {
-    console.log('Request URL:', req.originalUrl)
-    console.log('Request Path:', req.path)
-    console.log('dir name:', __dirname)
     res.sendFile(path.join(__dirname+'/index.html'));
   })
 
